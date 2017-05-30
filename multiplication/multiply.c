8fc82f61544f78c94f9f38bbd1f664904115a58a
koincidencia <kazykazy91@gmail.com>
2017-05-30 20:22:09 +0200

    add all

---
+++ multiplication/multiply.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * Create a function that prints a multiplication table with a given base number.
+ * It should take the base number as a parameter, and print the output to the console.
+ *
+ * The output should look like this for 5 as base:
+ *   1 * 5 = 5
+ *   2 * 5 = 10
+ *   3 * 5 = 15
+ *   4 * 5 = 20
+ *   5 * 5 = 25
+ *   6 * 5 = 30
+ *   7 * 5 = 35
+ *   8 * 5 = 40
+ *   9 * 5 = 45
+ *   10 * 5 = 50
+ *
+ * Non-positive base numbers should be treated as errors, handle it!
+ */
+
+int main() {
+
+  return 0;
+}
